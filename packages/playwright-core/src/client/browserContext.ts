6a4f5372919f22004635f2f8e1d4a450886949bb
Pavel Feldman <pavel.feldman@gmail.com>
2024-05-21 15:22:08 -0700

    fix: propagate close reason to api context

--- packages/playwright-core/src/client/browserContext.ts
+++ packages/playwright-core/src/client/browserContext.ts
@@ -445,6 +445,7 @@ export class BrowserContext extends ChannelOwner<channels.BrowserContextChannel>
       return;
     this._closeReason = options.reason;
     this._closeWasCalled = true;
+    await this.request.dispose(options);
     await this._wrapApiCall(async () => {
       await this._browserType?._willCloseContext(this);
       for (const [harId, harParams] of this._harRecorders) {
